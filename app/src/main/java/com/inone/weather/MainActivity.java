e985f8cfa903bd6c7b820bb181506a8f3e5dc1e6
Yonghong.Xu <Yonghong.Xu@Lavaszx.com>
2019-12-30 09:00:34 +0800

    省县市数据的加载

---
+++ app/src/main/java/com/inone/weather/MainActivity.java
@@ -0,0 +1,21 @@
+package com.inone.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * db:存放数据库模型相关代码
+ * gson：存放GSON模型相关代码
+ * service：存放服务相关代码
+ * util：存放工具相关代码
+ * */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/inone/weather/util/HttpUtil.java
@@ -0,0 +1,30 @@
+package com.inone.weather.util;
+
+import android.util.Log;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+/*
+*和服务器交互
+ */
+public class HttpUtil {
+    private static final String TAG = "HttpUtil";
+    public static  void sendOkHttpRequest(String address,okhttp3.Callback callback){//传入地址，注册回调来处理服务器响应
+        Log.d(TAG, "sendOkHttpRequest:************************address***** " +address );
+        //创建OkHttpClient实例
+        OkHttpClient client = new OkHttpClient();
+        //创建Request，发起一条http请求
+        Request request = new Request.Builder().url(address).build();
+        Log.d(TAG, "sendOkHttpRequest: ******request********" +request);
+        //newCall()方法创建一个call对象，
+        client.newCall(request).enqueue(callback);
+        Log.d(TAG, "sendOkHttpRequest: **************client.newCall(request).enqueue(callback);******" + client.newCall(request));
+
+    }
+
+//    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){  //发起Http请求调用sendOkHttpRequest方法（传入请求地址，注册一个回调来处理服务器响应）
+//        OkHttpClient client = new OkHttpClient();  //OkHttpClient实例
+//        Request request = new Request.Builder().url(address).build(); //Request请求创建
+//        client.newCall(request).enqueue(callback); //同步请求
+//    }
+}
